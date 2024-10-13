a35d3b7ee32cd68f0cdb2299d7caf62f82b2af86
MubashirSaeed <mubashirsono942@gmail.com>
2024-10-14 02:38:59 +0500

    update [blogId]

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -5,19 +5,28 @@ interface BlogPageParams {
   blogId: number;
 }
 
-interface Post {
+
+
+interface PostSpecificData {
   userId: number;
   id: number;
   title: string;
   body: string;
 }
-interface CommentInterFace {
+
+interface CommentData {
   postId: number;
   id: number;
   name: string;
   email: string;
   body: string;
 }
+
+interface Data {
+  postSpecificData: PostSpecificData;
+  specificCommentData: CommentData[];
+}
+
 const getSpecificData = async ({ params }: { params: BlogPageParams }) => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.blogId}`)
   if (response.ok) {
@@ -29,14 +38,16 @@ const getSpecificData = async ({ params }: { params: BlogPageParams }) => {
       postSpecificData: sepicificData,
       specificCommentData: commentsResponse
     }
-
   }
 
 }
   export default async function Blogpage({ params }: { params: BlogPageParams }) {
 
-    const data: any = await getSpecificData({params})
-    console.log("data", data)
+    const data : Data | undefined = await getSpecificData({params})
+    if (!data) {
+      return <div>Error: Data not found!</div>;
+    }
+    
 
   
   return (
@@ -47,7 +58,7 @@ const getSpecificData = async ({ params }: { params: BlogPageParams }) => {
       </div>
       <div className="bg-gray-100 p-4">
         <h3 className="text-lg font-semibold mb-2">
-          {data.specificCommentData.map((com: CommentInterFace) => {
+          {data.specificCommentData.map((com: CommentData) => {
             return (
               <div key={com.id}>
                 <Comments PostComments={com} />
